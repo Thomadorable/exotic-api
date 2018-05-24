d2c23ebd695591a391152ac9f1ee11202db36783
Thomas Deroua <thomasderoua@hotmail.com>
2018-05-24 11:18:51 +0200

    merge

--- conf.js
+++ conf.js
@@ -2,10 +2,10 @@ const isProd = false;
 
 const localConfif = {
     user: "root",
-    //password: "",
+    password: "root",
     database: "exotic",
-    //port: "8889"
-    port: "3306"
+    port: "8889"
+    // port: "3306"
 };
 
 if (isProd) {
